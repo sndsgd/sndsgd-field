35ec867e4264c3e34e75f5b1eba03dfe63c70c22
Russell <r@snds.gd>
2015-01-21 08:02:01 -0500

    renames EmptyDirectoryRule to WritableDirectoryRule
    allows the value to be a directory that does not yet exist

---
+++ src/field/rule/WritableDirectoryRule.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace sndsgd\field\rule;
+
+use \InvalidArgumentException;
+use \sndsgd\Dir;
+use \sndsgd\Path;
+
+
+/**
+ * Ensure a value is an empty directory
+ *
+ * Note: if you use this rule, you should use sndsgd\field\rule\PathTestRule 
+ * first to ensure the value is actually a directory
+ */
+class WritableDirectoryRule extends \sndsgd\field\Rule
+{
+   /**
+    * {@inheritdoc}
+    */
+   protected $message = "'{{path}}' must be an empty directory";
+
+   /**
+    * Whether or not the provided path will be a directory
+    *
+    * @var boolean
+    */
+   protected $mustBeEmpty = false;
+
+   /**
+    * @param boolean $mustBeEmpty
+    */
+   public function __construct($mustBeEmpty = false)
+   {
+      if (!is_bool($mustBeEmpty)) {
+         throw new InvalidArgumentException(
+            "invalid value provided for 'mustBeEmpty'; ".
+            "expecting a boolean"
+         );
+      }
+      $this->mustBeEmpty = $mustBeEmpty;
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   public function getMessage()
+   {
+      return str_replace('{{path}}', $this->value, $this->message);
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   public function validate()
+   {
+      if (($test = Dir::isWritable($this->value)) !== true) {
+         $this->message = $test;
+         return false;
+      }
+      else if (
+         file_exists($this->value) && 
+         $this->mustBeEmpty === true && 
+         count(scandir($this->value)) !== 2
+      ) {
+         $this->message = "'{$this->value}' must be an empty directory";
+         return false;
+      }
+      return true;
+   }
+}
+
--- tests/field/rule/WritableDirectoryRuleTest.php
+++ tests/field/rule/WritableDirectoryRuleTest.php
@@ -6,7 +6,7 @@ use \StdClass;
 use \org\bovigo\vfs\vfsStream;
 
 
-class EmptyDirectoryRuleTest extends \PHPUnit_Framework_TestCase
+class WritableDirectoryRuleTest extends \PHPUnit_Framework_TestCase
 {
    public function setUp()
    {
@@ -20,29 +20,62 @@ class EmptyDirectoryRuleTest extends \PHPUnit_Framework_TestCase
             'file.txt' => 'contents',
             'file2.txt' => 'contents',
          ],
+         'not-writable' => [
+            'file.txt' => 'contents'
+         ],
+         'empty-not-writable' => [],
       ]);
+
+      foreach (['not-writable', 'empty-not-writable'] as $dir) {
+         $this->root->getChild($dir)
+            ->chmod(0700)
+            ->chown(vfsStream::OWNER_ROOT)
+            ->chgrp(vfsStream::GROUP_ROOT);
+      }
+   }
+
+   /**
+    * @expectedException InvalidArgumentException
+    */
+   public function testConstructorInvalidArg()
+   {
+      $rule = new WritableDirectoryRule(42);
    }
 
    public function testSuccess()
    {
-      $rule = new EmptyDirectoryRule;
+      $rule = new WritableDirectoryRule;
 
       $url = vfsStream::url('root/empty-dir');
       $rule->setValue($url);
       $this->assertTrue($rule->validate());
-      $this->assertEquals($url, $rule->getValue());
+
+      $url = vfsStream::url('root/one-child');
+      $rule->setValue($url);
+      $this->assertTrue($rule->validate());
    }
 
    public function testFailure()
    {
-      $rule = new EmptyDirectoryRule;
+      $rule = new WritableDirectoryRule;
+
+      $url = vfsStream::url('root/empty-not-writable');
+      $rule->setValue($url);
+      $this->assertFalse($rule->validate());
+
+      $url = vfsStream::url('root/empty-not-writable');
+      $rule->setValue($url);
+      $this->assertFalse($rule->validate());
+
+
+      $rule = new WritableDirectoryRule(true);
 
       $url = vfsStream::url('root/one-child');
       $rule->setValue($url);
       $this->assertFalse($rule->validate());
       $msg = "'$url' must be an empty directory";
       $this->assertEquals($msg, $rule->getMessage());
-      
+
       $url = vfsStream::url('root/multiple-children');
       $rule->setValue($url);
       $this->assertFalse($rule->validate());
